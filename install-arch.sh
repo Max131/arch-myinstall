71e5bc09f9aa976f397df26dcad7775451fa407d
Max131 <max_cable@hotmail.com>
2019-11-21 23:05:23 -0600

    Updated scripts and skel

--- install-arch.sh
+++ install-arch.sh
@@ -4,16 +4,16 @@ timedatectl set-ntp true
 clear
 echo "##### Instalando sistema base #####"
 sleep 2s
-pacstrap /mnt base base-devel linux-zen linux-zen-headers linux-firmware grub os-prober networkmanager vim mlocate sudo zsh pulseaudio pulsemixer alsa-utils unzip unrar nethogs man-db man-pages amd-ucode arch-wiki-lite arch-wiki-docs
+pacstrap /mnt base base-devel linux-zen linux-zen-headers linux-firmware grub os-prober networkmanager vim mlocate sudo zsh pulseaudio pulsemixer alsa-utils unzip unrar nethogs man-db man-pages amd-ucode arch-wiki-lite arch-wiki-docs terminus-font 
 
 clear
 echo "##### Instalando X y Openbox #####"
 sleep 2s
-pacstrap /mnt xorg-server xorg-xinit xorg-xrandr xorg-xkill xf86-input-libinput xf86-video-amdgpu terminus-font lightdm lightdm-gtk-greeter accountsservice openbox obconf nitrogen tint2 jgmenu elementary-icon-theme arc-gtk-theme lxappearance lxtask guake volumeicon parcellite picom file-roller ttf-droid ttf-roboto ttf-fira-code ttf-inconsolata ttf-opensans firefox-i18n-es-mx network-manager-applet xcape xdotool gsimplecal evince libreoffice-fresh libreoffice-fresh-es gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gvfs gvfs-mtp gvfs-nfs xdg-user-dirs gmrun vlc code filezilla qbittorrent telegram-desktop gparted htop gpick sk1 gimp conky-manager geary pantheon-calculator pantheon-music pantheon-videos pantheon-calendar pantheon-photos pantheon-code pantheon-screenshot pantheon-terminal pantheon-files pantheon-polkit-agent dunst git lxmenu-data nicotine+ htop qt5ct
+pacstrap /mnt xorg-server xorg-xinit xorg-xrandr xorg-xkill xf86-input-libinput xf86-video-amdgpu lightdm lightdm-gtk-greeter accountsservice openbox obconf nitrogen tint2 jgmenu elementary-icon-theme arc-gtk-theme lxappearance lxtask guake volumeicon parcellite picom file-roller ttf-droid ttf-roboto ttf-fira-code ttf-inconsolata ttf-opensans firefox-i18n-es-mx network-manager-applet xcape xdotool gsimplecal evince libreoffice-fresh libreoffice-fresh-es gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gvfs gvfs-mtp gvfs-nfs xdg-user-dirs gmrun vlc code filezilla qbittorrent telegram-desktop gparted htop gpick sk1 gimp conky-manager geary pantheon-calculator pantheon-music pantheon-videos pantheon-calendar pantheon-photos pantheon-code pantheon-screenshot pantheon-terminal pantheon-files pantheon-polkit-agent dunst git lxmenu-data nicotine+ htop qt5ct
 
 genfstab -U /mnt >> /mnt/etc/fstab
 cp post-install.sh /mnt/root/
 echo "Cambiando a chroot /mnt"
-echo "Conituna ejecutando el script post-install.sh en /roo"
+echo "Conituna ejecutando el script post-install.sh en /root"
 
 arch-chroot /mnt
--- post-install.sh
+++ post-install.sh
@@ -7,23 +7,51 @@ sed -i 's/# es_MX/es_MX/g' locale.gen
 #echo "es_MX ISO-8859-1" >> /etc/locale.gen
 locale-gen
 echo "LANG=es_MX.UTF-8" > /etc/locale.conf
-echo "KEYMAP=es" > /etc/vconsole.conf
-echo "max" > /etc/hostname
-echo "127.0.0.1		max" >> /etc/hosts
-echo "::1			max" >> /etc/hosts
-echo "127.0.0.1		max.localdomain		max" >> /etc/hosts
+echo "KEYMAP=es" >> /etc/vconsole.conf
+echo "FONT=ter-i18n" >> /etc/vconsole.conf
+
+read -p "¿Nombre del PC? [max]: " myhost
+myhost=${myhost:-max}
+
+echo $myhost > /etc/hostname
+echo "127.0.0.1		$myhost" >> /etc/hosts
+echo "::1			$myhost" >> /etc/hosts
+echo "127.0.0.1		$myhost.localdomain		$myhost" >> /etc/hosts
+
+sed -i 's/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck consolefont/g'
+
 mkinitcpio -P
+
+echo "##########################################"
+echo "#### Configurando contraseña de root  ####"
+echo "##########################################"
+
 passwd
+
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
-useradd -u 1001 -m -G audio,video,optical,storage,disk,lp,wheel,users,network,power m13
-passwd m13
+
+read -p "¿Nombre de usuario? [m13]: " myuser
+myuser=${myuser:-m13}
+
+useradd -u 1001 -m -G audio,video,optical,storage,disk,lp,wheel,users,network,power $myuser
+
+echo "##########################################"
+echo "## Configurando contraseña del usuarop ###"
+echo "##########################################"
+
+passwd $myuser
+
 echo "EDITOR=vim" >> /etc/environment
 echo "BROWSER=firefox" >> /etc/environment
-echo "tint2 &" >> /etc/xdg/openbox/autostart
-echo "guake &" >> /etc/xdg/openbox/autostart
-echo "nm-applet &" >> /etc/xdg/openbox/autostart
-echo "parcellite &" >> /etc/xdg/openbox/autostart
-echo "volumeicon &" >> /etc/xdg/openbox/autostart
+
+cat>/etc/xdg/openbox/autostart<<EOF -
+tint2 &
+guake &
+nm-applet &
+parcellite &
+volumeicon &
+EOF
+
 echo "export XDG_CURRENT_DESKTOP=openbox" >> /etc/xdg/openbox/environment
